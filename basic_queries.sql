9d43f1672ab1df1aa42fc115b9799269fb1a2ac1
fullstackmedusa <alo.maldonado96@gmail.com>
2024-07-31 19:28:42 -0700

    query solutions

--- basic_queries.sql
+++ basic_queries.sql
@@ -3,29 +3,39 @@
 
 -- Get all information about all authors
 
+  SELECT * FROM authors;
+
 -- Get just the name and birth year of all authors
 
--- Get all authors born in the 20th centruy or later
+  SELECT name, birth_year FROM authors;
+
+-- Get all authors born in the 20th century or later
+
+  SELECT * FROM authors WHERE birth_year >= 1900;
 
 -- Get all authors born in the USA
-
+  SELECT * FROM authors WHERE nationality = 'United States of America';
 -- Get all books published on 1985
-
+SELECT * FROM books WHERE publication_date = 1985;
 -- Get all books published before 1989
-
+SELECT * FROM books WHERE publication_date <  1989;
 -- Get just the title of all books.
-
+SELECT title FROM books;
 -- Get just the year that 'A Dance with Dragons' was published
   -- Cry when you realize how long it's been
-
+SELECT publication_date FROM books WHERE title = 'A Dance with Dragons';
 -- Get all books which have `the` somewhere in their title (hint, look up LIKE/ILIKE)
-
+SELECT * FROM books WHERE title LIKE '%the%';
 -- Add yourself as an author
-
+INSERT INTO authors (name, nationality, birth_year) VALUES ('Alondra Salazar', 'United States of America', 1996);
 -- Add two books that you'd like to write (you can hard-code your id as the author id)
+INSERT INTO books (title, publication_date, author_id) VALUES ('Python for gerbils', 2024, 1), ('How do nothing while doing everything', 2024, 1);
 
 -- Update one of your books to have a new title
-
+UPDATE books SET title = 'Python for hamsters' WHERE title = 'Python for gerbils';
 -- Delete your books
 
+ DELETE FROM books WHERE author_id = 1;
+
 -- Delete your author entry
+DELETE FROM authors WHERE name = 'Alondra Salazar';
